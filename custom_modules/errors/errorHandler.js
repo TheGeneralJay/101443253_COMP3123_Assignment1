d870d2c57df2c42d4c68f6cda4861c0be63652bb
Amanda <amandagurns@hotmail.ca>
2024-10-08 12:45:36 -0400

    Implemented error handling for signup.

---
+++ custom_modules/errors/errorHandler.js
@@ -0,0 +1,12 @@
+// Error handling variables.
+const DEFAULT_ERROR = {
+    "status": false,
+    "message": "An error has occurred."
+}
+
+const EMPTY_INPUT_ERROR = {
+    "status": false,
+    "message": "Input parameters cannot be empty."
+}
+
+module.exports = {DEFAULT_ERROR, EMPTY_INPUT_ERROR};
--- custom_modules/routes/signupRoutes.js
+++ custom_modules/routes/signupRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("../db.js");
 const router = express.Router();
+const errors = require("../errors/errorHandler.js");
 
 // ---------------------------------------------
 // DB Connection
@@ -17,18 +18,45 @@ db.mongoose.connect(db.uri);
 
 router.post("/", async (req, res) => {
     // Create a new user using the User schema.
-    const user = new db.user(req.body);
+    let { username, email, password } = req.body;
 
-    // Save the new user.
-    await user.save();
-
-    // Generate the success response.
-    response = {
-        "message": "User created successfully.",
-        "user_id": user._id
+    // Empty params handling.
+    try {
+        // If any of the input params are empty, throw an error.
+        if (username == undefined || email == undefined || password == undefined) {
+            throw new Error();
+        }
+    } catch (err) {
+        res.status(400).send(errors.EMPTY_INPUT_ERROR);
+        return;
     }
-    // Send status of 201 and JSON response.
-    res.status(201).json(response);
+    
+    // If we get here, we can create the user.
+    try {
+        // Grab the current date.
+        const createdAt = new Date().toLocaleDateString();
+
+        const user = new db.user({
+            username: username,
+            email: email,
+            password: password,
+            created_at: createdAt
+        });
+
+        // Save the new user.
+        await user.save();
+
+        // Generate the success response.
+        response = {
+            "message": "User created successfully.",
+            "user_id": user._id
+        }
+        // Send status of 201 and JSON response.
+        res.status(201).json(response);
+    } catch (err) {
+        // Send a default error if anything above fails.
+        res.status(400).send(errors.DEFAULT_ERROR);
+    }
 });
 
 module.exports = router;
