a333e078f595ae04b4ddcd23f218546816d68f0c
Amanda <amandagurns@hotmail.ca>
2024-11-30 15:21:03 -0500

    adjustment

--- app.js
+++ app.js
@@ -2,20 +2,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.use(express.json());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   const origin = "https://employee-management-56b09.firebaseapp.com/";
-
-  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
+
+app.use(express.json());
 
 // Define server paths for ease of use.
 const signupPath = "/api/v1/user/signup";
