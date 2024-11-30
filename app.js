b71be3ccdc94d7f7c4eec6a6cb074a673d92b875
Amanda <amandagurns@hotmail.ca>
2024-11-30 14:59:07 -0500

    minor update

--- app.js
+++ app.js
@@ -3,22 +3,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://employee-management-56b09.web.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-});
 
 // Define server paths for ease of use.
 const signupPath = "/api/v1/user/signup";
